9a6f2b498410b36996d6708e687dff84da188ffa
Dhayv <davidhyppolitedh@gmail.com>
2024-07-15 21:47:07 -0400

    code: login button added

--- front/src/Header.jsx
+++ front/src/Header.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import { Container, Button } from "react-bootstrap";
+import NavBar from "./NavBar";
 
 const Header = () => {
-
     return (
-        <div> Welcom to Team 25</div>
-    )
+        <Container>
+                <div className="ml-auto d-flex">
+                    <Button variant="dark" className="me-2">Dark</Button>
+                    <Button variant="dark">Login</Button>
+                </div>
+
+        </Container>
+    );
 }
 
 export default Header;
