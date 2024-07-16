31a3e232a1564799cf1303528930c32ced0a5f92
Dhayv <davidhyppolitedh@gmail.com>
2024-07-15 22:21:01 -0400

    code: generic endpoint code

--- front/src/Login.jsx
+++ front/src/Login.jsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from 'axios';
 
 
 const Login = () => {
+  
+        
+    const LoginUser = () => {
+         axios.post(`/login`)
+            .then(response => {
+                console.log("Login Successful:", response.data)
+
+            })
+            .catch(error => { 
+                console.log("Cant login:", error)
+                
+            })
+    }}
 
     return(
         <section>Login </section>
     )
-}
+
 
 export default Login;
--- front/src/SignUp.jsx
+++ front/src/SignUp.jsx
@@ -1,12 +1,32 @@
-import React from "react";
+import axios from "axios";
+import React, {useState} from "react";
 
 
 const SignUp = () => {
 
+    const [formData, setFormData] = useState({
+        username: '',
+        email: '',
+        password: '',
+    })
+
+    const CreateUser = async () => {
+            await axios.post('/createUser')
+                .then(response => 
+                    {
+                        setFormData(response.data)
+                    })
+                .catch(error => {
+                    console.error("Cant create account:", error);
+                });
+            };
+   
+        };
+        
     return(
         <section>Signup</section>
     )
-}
+
 
 
 export default SignUp;
