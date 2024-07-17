2fd1974b7c2cae42fc77abf959b0a31d2fe96de0
Dhayv <davidhyppolitedh@gmail.com>
2024-07-16 22:11:29 -0400

    refactor: try/catch

--- front/src/SignUp.jsx
+++ front/src/SignUp.jsx
@@ -3,30 +3,65 @@ import React, {useState} from "react";
 
 
 const SignUp = () => {
-
+    const [message, setMessage] = useState('')
     const [formData, setFormData] = useState({
-        username: '',
+        userName: '',
         email: '',
-        password: '',
+        passWord: '',
     })
-
-    const CreateUser = async () => {
-            await axios.post('/createUser')
-                .then(response => 
-                    {
-                        setFormData(response.data)
-                    })
-                .catch(error => {
-                    console.error("Cant create account:", error);
-                });
-            };
-   
         };
+
+        const handleSubmit = async (e) => {
+            e.preventdefault();
+            const {email, userName, passWord} = formData
+                try {
+                    const response = await axios.post('/createUser',{
+                        username: userName,
+                        email: email,
+                        password: passWord
+                    });
+
+                    if(response.status === 201) {
+                        setMessage("User Created Successfully.")
+                        setFormData({
+                            userName: '',
+                            email: '',
+                            passWord: ''
+                        })
+                    }
+
+                } catch (error) {
+                    console.error("Cant create account:", error);
+                };
+
+
         
     return(
-        <section>Signup</section>
+        <div>
+            <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+                </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                <Form.Check type="checkbox" label="Check me out" />
+            </Form.Group>
+            <Button variant="primary" type="submit" oncli>
+                Submit
+            </Button>
+            </Form>
+
+        </div>
     )
 
-
+}
 
 export default SignUp;
