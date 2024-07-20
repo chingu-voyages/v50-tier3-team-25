6419bd22c0de4704ba6b46b3d705f1110be3bbd1
Dhayv <davidhyppolitedh@gmail.com>
2024-07-20 13:36:44 -0400

    save point

--- front/src/MapLocations.jsx
+++ front/src/MapLocations.jsx
@@ -3,19 +3,29 @@ import {Container, Col, Row} from "react-bootstrap"
 import {APIProvider, Map, AdvancedMarker} from '@vis.gl/react-google-maps';
 
 
+
+
 const MapLocations = () => {
     const googleKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
     const center = {lat: 39.8283, lng: -98.5795};
 
+    // map through menu locations eg. city and state
     const locations = []
 
+
+    // transform locations into geo data
+    const geoLocations = []
+
+    co
+
     return(
-        <Container className="justify-content-center">
+        <Container className="d-flex align-items-center justify-content-center flex-column">
             <APIProvider apiKey={googleKey} >
-                <Row className="align-items-center" >Locations Across the USA</Row>
-                <div style={{height: '50vh', width: '75%'}}>
-                <Map defaultCenter={center} defaultZoom={5}>
-                    <AdvancedMarker position={center}/>
+                <Row>Locations Across the USA</Row>
+                <div style={{height: '75vh', width: '75%', margin: '0 auto'}}>
+                <Map defaultCenter={center} defaultZoom={4.5}>
+                  
+                    {<AdvancedMarker position={center}/>}
                 </Map>
                 </div>
             </APIProvider>
