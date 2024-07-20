857b8b6394c99a890174d2572a92c4b38e805835
Dhayv <davidhyppolitedh@gmail.com>
2024-07-20 13:21:38 -0400

    code: intial build

--- front/src/MapLocations.jsx
+++ front/src/MapLocations.jsx
@@ -1,12 +1,26 @@
 import React from "react";
+import {Container, Col, Row} from "react-bootstrap"
+import {APIProvider, Map, AdvancedMarker} from '@vis.gl/react-google-maps';
 
 
 const MapLocations = () => {
+    const googleKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+    const center = {lat: 39.8283, lng: -98.5795};
+
+    const locations = []
 
     return(
-        <div>
-            <h4>Locations Across the USA</h4>
-        </div>
+        <Container className="justify-content-center">
+            <APIProvider apiKey={googleKey} >
+                <Row className="align-items-center" >Locations Across the USA</Row>
+                <div style={{height: '50vh', width: '75%'}}>
+                <Map defaultCenter={center} defaultZoom={5}>
+                    <AdvancedMarker position={center}/>
+                </Map>
+                </div>
+            </APIProvider>
+        </Container>
+        
     )
 }
 
