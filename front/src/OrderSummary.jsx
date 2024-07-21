3fe516219604788f117ea54d18c3e58b89bfd1b3
Dhayv <davidhyppolitedh@gmail.com>
2024-07-20 21:29:53 -0400

    code: basic summary

--- front/src/OrderSummary.jsx
+++ front/src/OrderSummary.jsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Container, Col, Row, Card, Button } from "react-bootstrap";
+import { getCart } from "./utility";
 
 const OrderSummary = () => {
+    const [cart, setCart] = useState([]);
+
+    useEffect(() => {
+        setCart(getCart());
+    }, []);
 
     return(
-        <div>
-            <h1>Order Summary Page</h1>
-        </div>
+        <Container>
+            <Card>
+                <Card.Body>
+                    <h6>Order Summary</h6>
+                    {cart.map((item) => 
+                    <ul 
+                        key={item.id}
+                    >
+                        <li>{item.name} - x{item.quantity} - ${item.price * item.quantity}</li>
+
+                    </ul>)
+                    }
+                    <Col>
+                        <Button> Checkout</Button>
+                    </Col>
+                </Card.Body>
+            </Card>
+        </Container>
     )
 }
 
