2d2d9c8eb69cf389f5602600b3ac13e5dfdda09e
SpookyLamb <turtleytyrant@gmail.com>
2024-07-26 14:27:53 -0400

    Basic profile modal.

--- front/src/Profile.jsx
+++ front/src/Profile.jsx
@@ -1,11 +1,38 @@
 import React from "react";
 
-const Profile = () => {
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 
+const Profile = ({auth, setView}) => {
+    
     return(
-        <div>
-            <h2> Profile Page</h2>
-            
+        <div className="modal-overlay">
+            <div className="modal-content">
+                <Container>
+                    <Row>
+                        <Col>
+                            <h2>Profile</h2>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            {"Username: " + auth.username}
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            {"Credits: " }
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <Button onClick={() => { setView(false) }}>OK</Button>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
         </div>
     )
 }
