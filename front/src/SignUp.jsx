77420dada0f582502caeb55fda6c5274cd1b5144
ecohen1125 <ecohen1125@gmail.com>
2024-08-01 15:30:51 -0500

    Fixed login and signup calls frontend

--- front/src/SignUp.jsx
+++ front/src/SignUp.jsx
@@ -1,129 +1,132 @@
 import axios from "axios";
 import React, { useState } from "react";
-import { Form, Button, Card, Container} from 'react-bootstrap';
-import {Link} from "react-router-dom";
-
-import { baseUrl } from "./api";
-import { secretKey } from "./api";
+import { Form, Button, Card, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
-    const [message, setMessage] = useState('');
-    const [formData, setFormData] = useState({
-        userName: '',
-        name: '',
-        email: '',
-        passWord: '',
-    });
+  const dbPassword = import.meta.env.VITE_SECRET_KEY;
+  const baseURL = import.meta.env.VITE_BASEURL;
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData(prevState => ({
-            ...prevState,
-            [name]: value
-        }));
-    };
+  const [message, setMessage] = useState("");
+  const [formData, setFormData] = useState({
+    username: "",
+    fullName: "",
+    email: "",
+    password: "",
+  });
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const { email, name, userName, passWord } = formData;
-        try {
-            const response = await axios(
-                {
-                    method: 'post',
-                    url: `${baseUrl}/createUser`,
-                    data: {
-                        username: userName,
-                        name: name,
-                        email: email,
-                        password: passWord,
-                        mongodbPassword: secretKey,
-                    },
-                }
-            )
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
 
-            if (response.status === 201) {
-                setMessage("User Created Successfully.");
-                setFormData({
-                    userName: '',
-                    name: '',
-                    email: '',
-                    passWord: ''
-                });
-            }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { email, username, password, fullName } = formData;
 
-        } catch (error) {
-            console.error("Can't create account:", error);
-            setMessage("Error creating account.");
-        }
-    };
+    try {
+      const response = await fetch(`${baseURL}/createUser`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: username,
+          name: fullName,
+          email: email,
+          password: password,
+          mongodbPassword: dbPassword,
+        }),
+      });
 
-    return (
-            <Container className="d-flex justify-content-center align-items-center vh-100">
-                <Card className="w-100" style={{ maxWidth: '400px'}}>
-                    <Card.Body>
-                    <h2 className="text-center mb-4">SignUp</h2>
-                    <Form onSubmit={handleSubmit}>
-                        
-                        <Form.Group className="mb-3" controlId="formBasicUsername">
-                            <Form.Label>Username</Form.Label>
-                            <Form.Control 
-                                type="text" 
-                                placeholder="Enter username" 
-                                name="userName"
-                                value={formData.userName}
-                                onChange={handleChange}
-                            />
-                        </Form.Group>
+      if (response.status === 201) {
+        setMessage("User Created Successfully.");
+        setFormData({
+          username: "",
+          email: "",
+          password: "",
+          fullName: "",
+        });
+      }
+    } catch (error) {
+      console.error("Can't create account:", error);
+      setMessage("Error creating account.");
+    }
+  };
 
-                        <Form.Group className="mb-3" controlId="formBasicName">
-                            <Form.Label>Name</Form.Label>
-                            <Form.Control 
-                                type="text" 
-                                placeholder="Enter Name" 
-                                name="name"
-                                value={formData.name}
-                                onChange={handleChange}
-                            />
-                        </Form.Group>
+  return (
+    <Container className="d-flex justify-content-center align-items-center vh-100">
+      <Card className="w-100" style={{ maxWidth: "400px" }}>
+        <Card.Body>
+          <h2 className="text-center mb-4">SignUp</h2>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="formBasicUsername">
+              <Form.Label>Username</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter Username"
+                name="username"
+                value={formData.userName}
+                onChange={handleChange}
+              />
+            </Form.Group>
 
-                        <Form.Group className="mb-3" controlId="formBasicEmail">
-                            <Form.Label>Email address</Form.Label>
-                            <Form.Control 
-                                type="email" 
-                                placeholder="Enter email" 
-                                name="email"
-                                value={formData.email}
-                                onChange={handleChange}
-                            />
-                        </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicName">
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter Name"
+                name="fullName"
+                value={formData.firstName}
+                onChange={handleChange}
+              />
+            </Form.Group>
 
-                        <Form.Group className="mb-3" controlId="formBasicPassword">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control 
-                                type="password" 
-                                placeholder="Password" 
-                                name="passWord"
-                                value={formData.passWord}
-                                onChange={handleChange}
-                            />
-                        </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter Email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+              />
+            </Form.Group>
 
-                        <Button variant="primary" type="submit">
-                            Submit
-                        </Button>
-                    </Form>
-                    {message && <p>{message}</p>}
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                name="password"
+                value={formData.passWord}
+                onChange={handleChange}
+              />
+            </Form.Group>
 
-                    <p className="text-center mt-3">
-                        <Link to="/login" className="btn btn-link" onClick={() => { useHref("/login") }}>
-                            Already have an account? Login
-                        </Link>
-                    </p>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+          {message && <p>{message}</p>}
 
-                    </Card.Body>
-                </Card>
-            </Container>
-    );
-}
+          <p className="text-center mt-3">
+            <Link
+              to="/login"
+              className="btn btn-link"
+              onClick={() => {
+                useHref("/login");
+              }}>
+              Already have an account? Login
+            </Link>
+          </p>
+        </Card.Body>
+      </Card>
+    </Container>
+  );
+};
 
 export default SignUp;
--- front/src/api.js
+++ front/src/api.js
@@ -1,63 +1,71 @@
 import axios from "axios";
 
-export const baseUrl = import.meta.env.VITE_BASEURL
-export const secretKey = import.meta.env.SECRET_KEY
+export const baseUrl = import.meta.env.VITE_BASEURL;
+export const secretKey = import.meta.env.SECRET_KEY;
 
-export const getMenu = ({setMenu}) => {
-    axios({
-        method: 'get',
-        url: `https://menus-api.vercel.app`,
-    }).then(response => {
-        setMenu(response.data)
-        console.log(response.data)
-    }).catch(error => {
-        console.log('ERROR: ', error)
+export const getMenu = ({ setMenu }) => {
+  axios({
+    method: "get",
+    url: `https://menus-api.vercel.app`,
+  })
+    .then((response) => {
+      setMenu(response.data);
+      console.log(response.data);
     })
-}
+    .catch((error) => {
+      console.log("ERROR: ", error);
+    });
+};
 
-export const getCredits = ({auth, setInformation}) => {
-    axios({
-        method: 'get',
-        url: `${baseUrl}/getCredits`,
-        body: {
-            username: auth.username,
-            mongodbPassword: secretKey,
-        }
-    }).then(response => {
-        console.log("RESPONSE: ", response)
-    }).catch(error => {
-        console.log('ERROR: ', error)
+export const getCredits = ({ auth, setInformation }) => {
+  axios({
+    method: "get",
+    url: `${baseUrl}/getCredits`,
+    body: {
+      username: auth.username,
+      mongodbPassword: secretKey,
+    },
+  })
+    .then((response) => {
+      console.log("RESPONSE: ", response);
     })
-}
+    .catch((error) => {
+      console.log("ERROR: ", error);
+    });
+};
 
-export const addCredits = ({auth, creditsToAdd, setInformation}) => {
-    axios({
-        method: 'put',
-        url: `${baseUrl}/addCredits`,
-        body: {
-            username: auth.username,
-            credits: creditsToAdd,
-            mongodbPassword: secretKey,
-        }
-    }).then(response => {
-        console.log("RESPONSE: ", response)
-    }).catch(error => {
-        console.log('ERROR: ', error)
+export const addCredits = ({ auth, creditsToAdd, setInformation }) => {
+  axios({
+    method: "put",
+    url: `${baseUrl}/addCredits`,
+    body: {
+      username: auth.username,
+      credits: creditsToAdd,
+      mongodbPassword: secretKey,
+    },
+  })
+    .then((response) => {
+      console.log("RESPONSE: ", response);
     })
-}
+    .catch((error) => {
+      console.log("ERROR: ", error);
+    });
+};
 
-export const useCredits = ({auth, creditsUsed, setInformation}) => {
-    axios({
-        method: 'put',
-        url: `${baseUrl}/useCredits`,
-        body: {
-            username: auth.username,
-            credits: creditsUsed,
-            mongodbPassword: secretKey,
-        }
-    }).then(response => {
-        console.log("RESPONSE: ", response)
-    }).catch(error => {
-        console.log('ERROR: ', error)
+export const useCredits = ({ auth, creditsUsed, setInformation }) => {
+  axios({
+    method: "put",
+    url: `${baseUrl}/useCredits`,
+    body: {
+      username: auth.username,
+      credits: creditsUsed,
+      mongodbPassword: secretKey,
+    },
+  })
+    .then((response) => {
+      console.log("RESPONSE: ", response);
     })
-}
+    .catch((error) => {
+      console.log("ERROR: ", error);
+    });
+};
