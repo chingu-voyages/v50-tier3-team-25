5d9359e4269ba82cfe3b74f1ed9a1d5c4343e750
ecohen1125 <ecohen1125@gmail.com>
2024-08-05 12:24:44 -0500

    Did more design

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,25 +1,50 @@
 import { Link } from "react-router-dom"
-
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import "./css/app.css";
 
 import MenuItem from "./MenuItem";
 
 const Title = () => {
   return (
-    <h1 className="text-center">
-      Team 25 Restaurant
+    <h1 className="text-center title">
+      Nom Nom Nexus
     </h1>
+  )
+}
+
+function HeroWithStoreFinder() {
+  return (
+    <Container className="hero-container position-relative mb-4">
+      <Row className="justify-content-center">
+        <Col xs={12} md={10} lg={8} className="p-0">
+          <div className="hero-image-wrapper">
+            <img 
+              src="./src/assets/FoodHeroImage.jpg"
+              alt="Delicious spread of our most popular dishes"
+              className="img-fluid rounded"
+            />
+            <div className="store-finder-overlay d-flex justify-content-center align-items-center">
+              <Link to={"/locations"} className="btn btn-primary btn-lg">FIND A LOCATION NEAR YOU</Link>
+            </div>
+          </div>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
 function Hero() {
   return (
-    <Container className="py-2">
+    <Container className="">
       <Row>
-        <Col className="border hero rounded bg-light text-center p-5">
-          INSERT HERO IMAGE HERE
+        <Col className="rounded">
+          <img 
+            src="./src/assets/FoodHeroImage.jpg"
+            alt="Delicious spread of our most popular dishes" 
+            className="img-fluid rounded"
+          />
         </Col>
       </Row>
     </Container>
@@ -28,10 +53,10 @@ function Hero() {
 
 function StoreFinder() {
   return (
-    <Container className="py-2">
+    <Container className="py-4">
       <Row>
-        <Col className="border rounded bg-light text-center p-5">
-          <Link to={"/locations"}>FIND A LOCATION NEAR YOU</Link>
+        <Col className="store-finder rounded p-5">
+          <Link to={"/locations"} className="btn btn-primary">FIND A LOCATION NEAR YOU</Link>
         </Col>
       </Row>
     </Container>
@@ -40,24 +65,25 @@ function StoreFinder() {
 
 function Featured() {
   return (
-    <Container className="py-2">
+    <Container className="py-4">
+      <h2 className="mb-4">Featured Dishes</h2>
       <Row>
-          {MenuItem({ 
-            itemName: "Featured Example",
-            id: "example",
-            description: "An example featured menu item.",
-            image: "",
-            price: 99.99,
-            rating: 5,
-          })}
-          {MenuItem({ 
-            itemName: "Featured Example",
-            id: "example",
-            description: "An example featured menu item.",
-            image: "",
-            price: 99.99,
-            rating: 5,
-          })}
+        {MenuItem({
+          itemName: "Featured Example",
+          id: "example",
+          description: "An example featured menu item.",
+          image: "",
+          price: 99.99,
+          rating: 5,
+        })}
+        {MenuItem({
+          itemName: "Featured Example",
+          id: "example",
+          description: "An example featured menu item.",
+          image: "",
+          price: 99.99,
+          rating: 5,
+        })}
       </Row>
     </Container>
   )
@@ -65,24 +91,25 @@ function Featured() {
 
 function PopularDishes() {
   return (
-    <Container className="py-2">
+    <Container className="py-4">
+      <h2 className="mb-4">Popular Dishes</h2>
       <Row>
-          {MenuItem({ 
-            itemName: "Popular Example",
-            id: "example",
-            description: "An example popular menu item.",
-            image: "",
-            price: 99.99,
-            rating: 5,
-          })}
-          {MenuItem({ 
-            itemName: "Popular Example",
-            id: "example",
-            description: "An example popular menu item.",
-            image: "",
-            price: 99.99,
-            rating: 5,
-          })}
+        {MenuItem({
+          itemName: "Popular Example",
+          id: "example",
+          description: "An example popular menu item.",
+          image: "",
+          price: 99.99,
+          rating: 5,
+        })}
+        {MenuItem({
+          itemName: "Popular Example",
+          id: "example",
+          description: "An example popular menu item.",
+          image: "",
+          price: 99.99,
+          rating: 5,
+        })}
       </Row>
     </Container>
   )
@@ -90,15 +117,13 @@ function PopularDishes() {
 
 function App() {
   return (
-    <div className="h-100 p-5 text-center">
+    <div className="app-container">
       <Title />
-      <Hero/>
-      <StoreFinder/>
+      <HeroWithStoreFinder/>
       <Featured/>
       <PopularDishes/>
     </div>
   )
 }
-
 
 export default App
